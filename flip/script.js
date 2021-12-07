af364a256767416a4b5bda376d4a8333ff80f2a2
techhq <egor11ero@gmail.com>
2021-12-07 22:02:30 +0300

    new ver

--- flip/script.js
+++ flip/script.js
@@ -2,3 +2,22 @@ function changeNick() {
     document.getElementById("flip").innerHTML = document.getElementById("field").value;
     document.getElementById("field").value = null;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+            document.getElementById("flip").innerHTML = document.getElementById("field").value;
+            document.getElementById("field").value = null;
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key == ' ') {
+            document.getElementById("field").focus()
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key == 'Escape') {
+            document.getElementById("field").blur()
+    }
+  });
